2b726f104d074aaed9a76d1047025822b723318f
rowen.lin <rowen.lin@gogolook.com>
2023-10-19 11:58:29 +0800

    create mutation: create user

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,15 @@
 import { UserList } from '../dummyData.js';
+import _ from 'lodash';
 
 export const resolvers = {
   Query: {
-    users() {
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = _.find(UserList, { id: Number(id) });
+      return user;
     },
   },
 };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -2,14 +2,30 @@ import { gql } from 'apollo-server';
 
 const typeDefs = gql`
   type User {
+    id: ID!
     name: String!
     username: String!
     age: Int!
     nationality: String!
+    friends: [User]
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User
+  }
+
+  # interface
+  input CreateUserInput {
+    name: String!
+    username: String!
+    age: Int!
+    # default use taiwan
+    nationality: String = "TAIWAN"
+  }
+
+  type Mutation {
+    createUser(input: CreateUserInput!): User!
   }
 `;
 
